f6019c421852c23a895b32abe292a4f0a229ab7a
cit <cit@ccc-r.de>
2009-08-17 15:55:54 +0200

    Fixed a bug that caused the application to ignore the filename for low quality

--- src/encodroyd.cs
+++ src/encodroyd.cs
@@ -278,7 +278,7 @@ class Encodroyd {
                 proc.StartInfo.FileName = "ffmpeg";
                 
                 if (lowQualityRadio.Active)
-                    proc.StartInfo.Arguments = " -i " + filename + " -s 480x320 -vcodec mpeg4 -acodec libfaac -ac 1 -ar 16000 -r 13 -ab 32000 -aspect 3:2 '" + outputpath + "/" + outputfilename.Substring(0, outputfilename.LastIndexOf(".")) + "[And-Low].mp4'";
+                    proc.StartInfo.Arguments = " -i \"" + filename + "\" -s 480x320 -vcodec mpeg4 -acodec libfaac -ac 1 -ar 16000 -r 13 -ab 32000 -aspect 3:2 '" + outputpath + "/" + outputfilename.Substring(0, outputfilename.LastIndexOf(".")) + "[And-Low].mp4'";
                 else if (highQualityRadio.Active)
                     proc.StartInfo.Arguments = " -i \"" + filename + "\" -s 480x320 -b 384k -vcodec libx264 -flags +loop+mv4 -cmp 256 -partitions +parti4x4+parti8x8+partp4x4+partp8x8+partb8x8 -subq 7 -trellis 1 -refs 5 -bf 0 -flags2 +mixed_refs -coder 0 -me_range 16 -g 250 -keyint_min 25 -sc_threshold 40 -i_qfactor 0.71 -qmin 10 -qmax 51 -qdiff 4 -acodec libfaac -r 13 \"" + outputpath + "/" + outputfilename.Substring(0, outputfilename.LastIndexOf(".")) + "[And-High].mp4\"";
 
